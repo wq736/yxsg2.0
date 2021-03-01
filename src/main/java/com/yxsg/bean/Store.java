157151a9ce83ef98326f74acda39abf09206159c
wq736 <74338332+wq736@users.noreply.github.com>
2021-03-01 12:46:49 +0800

    Update Store.java

--- src/main/java/com/yxsg/bean/Store.java
+++ src/main/java/com/yxsg/bean/Store.java
@@ -5,10 +5,10 @@ import lombok.Data;
 public class Store {
     private Integer stoId;		//店铺id
     private String stoName;		//店铺名称
-    private String stoAddress;	//地址
-    private String stoMainShop;	//区域
+    private String stoAddress;		//地址
+    private String stoMainShop;		//区域
     private String stoTime;		//创建时间
-    private String stoStatus;	//店铺的状态
+    private String stoStatus;		//店铺的状态
     private Integer uId;		//所属用户id
     public Store() {}
 	public Store(Integer stoId, String stoName, String stoAddress, String stoMainShop, String stoTime, String stoStatus,
